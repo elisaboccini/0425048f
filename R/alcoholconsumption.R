1fbf39a7f4a414543660725f2a25382574e9a594
FrancescaGraziaRadatti <francescaradatti97@gmail.com>
2020-02-05 14:57:03 +0100

    testing

---
+++ R/alcoholconsumption.R
@@ -0,0 +1,5 @@
+#data acquisition
+x <- read.csv('drinks.csv')             # read file 
+head(x)  
+
+save(x,file="/Users/francescagraziaradatti/Documents/GitHub/0425048f/data/alcoholconsumption.rda")
