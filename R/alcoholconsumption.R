11fff31fd5646ff7d9df96d07f9ffde849913d49
FrancescaGraziaRadatti <francescaradatti97@gmail.com>
2020-02-05 18:26:16 +0100

    testing

--- R/alcoholconsumption.R
+++ R/alcoholconsumption.R
@@ -1,30 +1,21 @@
 #data acquisition
-x <- read.csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/alcohol-consumption/drinks.csv', sep=',')
-head(x)  
+alcoholconsumption <- read.csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/alcohol-consumption/drinks.csv', sep=',')
+head(alcoholconsumption)  
 #add a rda file
-save(x,file="/Users/francescagraziaradatti/Documents/GitHub/0425048f/data/alcoholconsumption.rda")
+save(alcoholconsumption,file="/Users/francescagraziaradatti/Documents/GitHub/0425048f/data/alcoholconsumption.rda")
+
 #data visualization1
-summary(x)
-plot(x)
-plot(x$country, x$total_litres_of_pure_alcohol)
+summary(alcoholconsumption)
+plot(alcoholconsumption)
+plot(alcoholconsumption$country, alcoholconsumption$total_litres_of_pure_alcohol)
 
 #data visualization2: install ggplot2 package
 install.packages("ggplot2", repos = "https://cran.rstudio.com")
 require(ggplot2)
-ggplot(x, aes(continent)) +
-  geom_bar(stat="count")
-ggplot(x, aes(x=country, y=total_litres_of_pure_alcohol, col=continent)) + geom_point()
+ggplot(alcoholconsumption, aes(country)) + geom_bar(stat="count")
+ggplot(alcoholconsumption, aes(x=country, y=total_litres_of_pure_alcohol, col=country)) + geom_point()
 
 
-#linear regression
-
-#view the structure of the dataframe
-str(x)
-
-#converse wine_servings and total_litres_of_pure_alcohol in numerical value
-x$winenum <- as.numeric(x$wine_servings)
-x$total_litresnum <- as.numeric(x$total_litres_of_pure_alcohol)
-str(x)
 
 
 
