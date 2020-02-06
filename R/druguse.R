a5c03800234b6861a55e20aab09de6edc6094feb
FrancescaGraziaRadatti <francescaradatti97@gmail.com>
2020-02-06 13:08:29 +0100

    committ

--- R/druguse.R
+++ R/druguse.R
@@ -2,10 +2,9 @@
 
 #data acquisition
 
-druguse <- read.csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/drug-use-by-age/drug-use-by-age.csv', sep=',')
+druguse <- read.csv('drug-use-by-age.csv')
 head(druguse)
-save(druguse,file="/Users/francescagraziaradatti/Documents/GitHub/0425048f/data/druguse.rda")
-str(druguse)
+
 
 
 
@@ -15,12 +14,12 @@ library(dplyr)
 
 p<- plot_ly(x="age", type="histogram")
 p
-
 #Data Analysis: sammury stats for all variables in the data set:
 summary(druguse)
 #Option 2: using subset
 druguse.sub0 <- subset(druguse, select=c("tranquilizer.use","marijuana.use"))
 summary(druguse.sub0)
+
 
 
 
