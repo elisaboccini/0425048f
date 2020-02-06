9fb995dabca8289f2d69e483967e8fa722c50ad9
FrancescaGraziaRadatti <francescaradatti97@gmail.com>
2020-02-06 11:51:16 +0100

    testing

--- R/druguse.R
+++ R/druguse.R
@@ -1,3 +1,5 @@
+
+
 #data acquisition
 
 druguse <- read.csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/drug-use-by-age/drug-use-by-age.csv', sep=',')
@@ -5,19 +7,22 @@ head(druguse)
 save(druguse,file="/Users/francescagraziaradatti/Documents/GitHub/0425048f/data/druguse.rda")
 str(druguse)
 
-#data visualization
 
-install.packages("ggplot2", repos = "https://cran.rstudio.com")
+
+#data visualization
+install.packages("ggplot2",repos = "https://cran.rstudio.com" )
 require(ggplot2)
 ggplot(druguse, aes(tranquilizer.use)) +
   geom_bar(stat="count")
 
+
 #Data Analysis: sammury stats for all variables in the data set:
 summary(druguse)
-
 #Option 2: using subset
 druguse.sub0 <- subset(druguse, select=c("tranquilizer.use","marijuana.use"))
 summary(druguse.sub0)
+
+
 
 
 
